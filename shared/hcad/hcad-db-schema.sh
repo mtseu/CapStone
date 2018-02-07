21e8ededb3226134f9f8904e68ebdb957b6ab207
Mikhail Tseu <mikhail_tseu@epam.com>
2018-02-07 11:16:14 +0000

    achange encoding of sql script from utf-16 to utf-8 add SNAPSHOT_YEAR column to each table parametrize the run of sql script with DBNAME

--- shared/hcad/hcad-db-schema.sh
+++ shared/hcad/hcad-db-schema.sh
@@ -4,18 +4,10 @@
 # and save it to the MS SQL Server database on Azure Cloud
 
 DEF_SERVER=azt-sql.eastus.cloudapp.azure.com
-DEF_DATABASE=default
 DEF_USER=azt
-DEF_PASS=AzureAdmin17
-
-function err {
-	echo $* >&2
-}
 
 SERVER=$DEF_SERVER
-DATABASE=$DEF_DATABASE
 USER=$DEF_USER
-PASS=$DEF_PASS
 
 while getopts "s:d:u:p:" ARG ; do
 	case $ARG in
@@ -39,9 +31,15 @@ echo DATABASE=$DATABASE
 echo USER=$USER
 echo PASS=$PASS
 
+if [ -z $PASS ]; then
+	echo "Password has to bw specified"
+	exit 1
+fi
+
 echo "Reinitializing the database schema"
 echo "sqlcmd -S $SERVER -U $USER -P $PASS -i create.sql"
-sqlcmd -S $SERVER -U $USER -P $PASS -i create.sql
+export DBNAME=$DATABASE 
+sqlcmd -S $SERVER -U $USER -P $PASS -i hcad-db-create.sql
 
 echo "Downloading files from HCAD"
 
