b559e84fca7a7dda93585d8fe5900ec99dd94230
Mikhail Tseu <mikhail_tseu@epam.com>
2018-02-13 09:56:46 +0300

    reflect moving to v2. make cwd-agnostic

--- shared/hcad/adf/v2/deploy-hcad-adf.sh
+++ shared/hcad/adf/v2/deploy-hcad-adf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 key=`az keyvault secret show --vault-name edap-keys --name edap-blob-key1 --output tsv | awk '{ print $6 }'`
 az storage container create \
     --account-name "edapblob" \
@@ -11,10 +13,10 @@ az storage blob upload \
     --account-key ${key} \
     --container-name "scripts" \
     --name "hive/add-snapshot_year.hql" \
-    --file "./add-snapshot_year.hql"
+    --file "${DIR}/../add-snapshot_year.hql"
 
 az group deployment create \
     --verbose \
     --resource-group "azure-training-shared" \
-    --template-file ./hcad-template.json \
-    --parameters @./hcad-params.json
+    --template-file "${DIR}/hcad-template.json" \
+    --parameters "@${DIR}/hcad-params.json"
