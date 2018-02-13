ae32ae62562f877bd4b79da52db444df58221dd0
Mikhail Tseu <mikhail_tseu@epam.com>
2018-02-13 10:06:04 +0300

    make it work for both v1 and v2

--- shared/hcad/adf/deploy-hcad-adf.sh
+++ shared/hcad/adf/deploy-hcad-adf.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "Say v1 or v2"
+    exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+VDIR=${DIR}/${1}
 
 key=`az keyvault secret show --vault-name edap-keys --name edap-blob-key1 --output tsv | awk '{ print $6 }'`
 az storage container create \
@@ -13,10 +20,10 @@ az storage blob upload \
     --account-key ${key} \
     --container-name "scripts" \
     --name "hive/add-snapshot_year.hql" \
-    --file "${DIR}/../add-snapshot_year.hql"
+    --file "${DIR}/add-snapshot_year.hql"
 
 az group deployment create \
     --verbose \
     --resource-group "azure-training-shared" \
-    --template-file "${DIR}/hcad-template.json" \
-    --parameters "@${DIR}/hcad-params.json"
+    --template-file "${VDIR}/hcad-template.json" \
+    --parameters "@${VDIR}/hcad-params.json"
