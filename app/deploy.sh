51a6424ee333387f980b73cba9d746e445be6233
Mikhail Tseu <mikhail_tseu@epam.com>
2018-03-05 14:54:27 +0300

    new hdi-conn; new TCAD tables; refactor deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+STORAGE_ACC="hdibloba"
+SCRIPTS_CONTAINER="scripts"
+
 az storage container create \
 --account-name "hdibloba" \
 --name "scripts"
 
-az storage blob upload \
---account-name "hdibloba" \
---container-name "scripts" \
---name "select/PROP.hql" \
---file "./scripts/select/PROP.hql"
+# uploads blob to Azure Blob Storage
+# 1 - account
+# 2 - container
+# 3 - blob
+# 4 - local file
+
+function upload-blob {
+    az storage blob upload \
+    --account-name "$1" \
+    --container-name "$2" \
+    --name "$3" \
+    --file "$4"
+}
+
+upload-blob $STORAGE_ACC $SCRIPTS_CONTAINER "select/PROP.hql" "./scripts/select/PROP.hql"
+upload-blob $STORAGE_ACC $SCRIPTS_CONTAINER "select/PROP_ENT.hql" "./scripts/select/PROP_ENT.hql"
+upload-blob $STORAGE_ACC $SCRIPTS_CONTAINER "select/TOTALS.hql" "./scripts/select/TOTALS.hql"
+
 
 az group deployment create  \
 --resource-group "msq-hdinsight-team-a" \
